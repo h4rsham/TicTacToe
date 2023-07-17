831ebb074f1149eedbc481ce9cc7b43b6ce5bf17
h4rsham <harsham.latif@outlook.com>
2023-07-17 13:08:05 +0100

    Added alternate player moves

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React, { useState } from 'react';
+
 import './App.css';
-// import Box from './components/box';
 import { Board } from './components/board';
 
 function App() {
-  const board = ["X","X","X","X","X","X","X","X","X"];
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlayer, setXPlayer] = useState(true);
+
+  const handleBoxClick = (boxIndex) => {
+    const updatedBoard = board.map((value, index) => {
+      if (index === boxIndex) {
+        return xPlayer === true ? 'X' : 'O';
+      } else {
+        return value;
+      }
+    });
+    setBoard(updatedBoard);
+
+    setXPlayer(!xPlayer);
+  };
+
+
+
   return (
     <div>
-      <Board board={board} onClick={null}/>
+      <Board board={board} onClick={handleBoxClick}/>
     </div>
   );
 }
