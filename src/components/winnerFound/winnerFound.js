8901be78243a7e36e53f0232faa0ebf9cbb2566e
h4rsham <harsham.latif@outlook.com>
2023-07-17 14:33:24 +0100

    Added conditions of winning message to have the css colours of the winner

--- src/components/winnerFound/winnerFound.js
+++ src/components/winnerFound/winnerFound.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./winnerFound.css";
 
 export const WinnerFoundText = ({ winner }) => {
+  const winnerClass = winner === "X" ? "x-winner" : "o-winner"; // this line sets the class of the div to either "x-winner" or "o-winner" depending on the value of the winner prop
   return (
-    <div className="winner-found">
+    <div className={`winner-found ${winnerClass}`}>
       <h1>{winner} wins!</h1>
     </div>
   );
